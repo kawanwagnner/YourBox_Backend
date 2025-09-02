b12da5ac2562422844a4ff0c7a96f90fe79623e3
Kawan Wnn | UY3 <kawan.silva@uy3.com.br>
2025-09-02 17:10:04 -0300

    [YourBox] feat: first contasct by postman its okay

--- src/http/routes/auth.routes.ts
+++ src/http/routes/auth.routes.ts
@@ -7,5 +7,8 @@ router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
 router.post('/logout', AuthController.logout);
 router.get('/me', AuthController.me);
+router.get('/alive', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 export default router;
